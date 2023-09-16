c3709ffa26cd09f206baed3a485ffeb14d19c006
Zachary Phillips <phillizr@udel.edu>
2023-09-16 11:30:33 -0400

    Set up file with questions and initial variables given by assignment.

--- Assignment3_data_type.py
+++ Assignment3_data_type.py
@@ -1,2 +1,51 @@
 #!/usr/bin/python
 
+# Setting up variables for questions 1-4 using positive integer values
+a = 12
+b = 5
+c = 28
+d = 17
+
+# Questions 4-6 use the following string, as provided by the assignment.
+S = "CGCAGTTGTATTGCTTCCCACATTTATTAGACCACCTATTAAAAATGGATTTCTTCCCATTTCAAGCTGCCCACAAATCTCGCTCCTGATACGTTCTTCACTTCAAGCCGTAGCATCCCAATATCAGAAGCGGCGCCGGACTTGTTTTCAAAATATCCACGTATCCCTTCTTTCTCTTTCAATAGAAAACACCCATTGGTTCCGAAATAACGCATCTTATACTGTGGCTTATTGGCGTTACCC"
+
+# Question 1: Assume right angle triangle with base = b and height = a. Return the length of the hypotenuse
+
+
+
+# Question 2: Assume a rectangle of length = c and width = d. 
+# 1. Return the area of the rectangle
+# 2. Return the perimeter of the rectangle
+
+
+
+
+# Question 3: Assume a circle where radius = a.
+# 1. Return the area of the circle
+# 2. Return the circumference of the circle.
+
+
+
+# Question 4: Given a string S of any length and four integers a, b, c, and d
+# 1. Slice the string S from indices a through b and c through d (with a space in between) inclusively
+# 2. Generate the reverse of string S
+
+
+
+# Question 5: Given a DNA string S
+# 1. Find first and last location of stop codon 'TAG' and report its starting locations
+# 2. Split the DNA string by stop codon 'TAG' and report wach fragment and their respective lengths
+# 3. Report percent GC of original DNA string S
+# 4. Report number of A, C, T, and G
+
+
+
+# Question 6: Given two lists
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+# 1. Return a list of all unique elements
+# 2. Return a list of all common elements
+# 3. Return a list of all elements
+
+
+
